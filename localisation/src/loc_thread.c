3a67a51b266783212864b51ef8d5daac921bdb65
Sami_aZeus <sami.ezzerouali@hotmail.com>
2019-02-06 01:21:45 +0100

    before test_make_tag

--- localisation/src/loc_thread.c
+++ localisation/src/loc_thread.c
@@ -19,11 +19,57 @@
 #include <errno.h>
 #include <pthread.h>
 
-#define outputFile "output.nodes"
 
+
+void config(){
+    dwm_cfg_tag_t cfg_tag;
+    dwm_cfg_t cfg_node;
+
+    HAL_Print("dwm_init(): dev%d\n", HAL_DevNum());
+    dwm_init();
+
+    HAL_Print("Setting to tag: dev%d.\n", HAL_DevNum());
+    cfg_tag.low_power_en = 0; 
+    cfg_tag.meas_mode = DWM_MEAS_MODE_TWR;
+    cfg_tag.loc_engine_en = 1;
+    cfg_tag.common.led_en = 1;
+    cfg_tag.common.ble_en = 1;
+    cfg_tag.common.uwb_mode = DWM_UWB_MODE_ACTIVE;
+    cfg_tag.common.fw_update_en = 0;
+    HAL_Print("dwm_cfg_tag_set(&cfg_tag): dev%d.\n", HAL_DevNum());
+    dwm_cfg_tag_set(&cfg_tag);
+
+    HAL_Print("Wait 2s for node to reset.\n");
+    HAL_Delay(2000);  
+    dwm_cfg_get(&cfg_node);
+
+    HAL_Print("Comparing set vs. get: dev%d.\n", HAL_DevNum());
+    if((cfg_tag.low_power_en        != cfg_node.low_power_en) 
+    || (cfg_tag.meas_mode           != cfg_node.meas_mode) 
+    || (cfg_tag.loc_engine_en       != cfg_node.loc_engine_en) 
+    || (cfg_tag.common.led_en       != cfg_node.common.led_en) 
+    || (cfg_tag.common.ble_en       != cfg_node.common.ble_en) 
+    || (cfg_tag.common.uwb_mode     != cfg_node.common.uwb_mode) 
+    || (cfg_tag.common.fw_update_en != cfg_node.common.fw_update_en))
+    {
+        HAL_Print("low_power_en        cfg_tag=%d : cfg_node=%d\n", cfg_tag.low_power_en,     cfg_node.low_power_en); 
+        HAL_Print("meas_mode           cfg_tag=%d : cfg_node=%d\n", cfg_tag.meas_mode,        cfg_node.meas_mode); 
+        HAL_Print("loc_engine_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.loc_engine_en,    cfg_node.loc_engine_en); 
+        HAL_Print("common.led_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.led_en,    cfg_node.common.led_en); 
+        HAL_Print("common.ble_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.ble_en,    cfg_node.common.ble_en); 
+        HAL_Print("common.uwb_mode     cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.uwb_mode,  cfg_node.common.uwb_mode); 
+        HAL_Print("common.fw_update_en cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.fw_update_en, cfg_node.common.fw_update_en);  
+        HAL_Print("\nConfiguration failed.\n\n");
+    }
+    else
+    {
+        HAL_Print("\nConfiguration succeeded.\n\n");
+    }
+   
+}
 
 void *loc_thread(void *mission_se){
-    
+
     pthread_exit(NULL);
 }
 
--- localisation/src/main.c
+++ localisation/src/main.c
@@ -1,78 +1,95 @@
-//////////////////////////////////////////////////////////
-//                                                      //
-//      (c)         : EZZEROUALI Sami 2018              //
-//      Tutor       : JENN Eric                         //
-//      Project     : CAPHCA                            //
-//      Program     : SPF - Shortest path first         //
-//                                                      //
-//////////////////////////////////////////////////////////
-
-
-#include "../include/header.h"
-#include "../include/loc_thread.h"
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h> // strerror
-#include <math.h>
-#include <errno.h>
-#include <pthread.h>
-
-
-
-
-#define outputFile "output.nodes"
-
-
-int main(int argc, char const *argv[])
+/*! ------------------------------------------------------------------------------------------------------------------
+ * @file    tag_cfg.c
+ * @brief   Decawave device configuration and control functions
+ *
+ * @attention
+ *
+ * Copyright 2017 (c) Decawave Ltd, Dublin, Ireland.
+ *
+ * All rights reserved.
+ *
+ */
+ 
+#include "dwm_api.h"
+#include "hal.h"
+      
+int main(void)
 {
-    int src,dest;
-    Path * trajectorytest;
-    // printf("%s , %s , %d",argv[0],argv[1],argc);
-    
-    
-    src=1;
-    dest=4;
-    
-    spf_mission mission_se={
-        .start = src,
-        .end = dest,
-        .path = trajectorytest,
-        .mut = PTHREAD_MUTEX_INITIALIZER
-    };
+   int i;
+   int wait_period = 1000;
+   dwm_cfg_tag_t cfg_tag;
+   dwm_cfg_t cfg_node;
+   
+   HAL_Print("dwm_init(): dev%d\n", HAL_DevNum());
+   dwm_init();
+   
+   HAL_Print("Setting to tag: dev%d.\n", HAL_DevNum());
+   cfg_tag.low_power_en = 0; 
+   cfg_tag.meas_mode = DWM_MEAS_MODE_TWR;
+   cfg_tag.loc_engine_en = 1;
+   cfg_tag.common.led_en = 1;
+   cfg_tag.common.ble_en = 1;
+   cfg_tag.common.uwb_mode = DWM_UWB_MODE_ACTIVE;
+   cfg_tag.common.fw_update_en = 0;
+   HAL_Print("dwm_cfg_tag_set(&cfg_tag): dev%d.\n", HAL_DevNum());
+   dwm_cfg_tag_set(&cfg_tag);
+   
+   HAL_Print("Wait 2s for node to reset.\n");
+   HAL_Delay(2000);  
+   dwm_cfg_get(&cfg_node);
+   
+   HAL_Print("Comparing set vs. get: dev%d.\n", HAL_DevNum());
+   if((cfg_tag.low_power_en        != cfg_node.low_power_en) 
+   || (cfg_tag.meas_mode           != cfg_node.meas_mode) 
+   || (cfg_tag.loc_engine_en       != cfg_node.loc_engine_en) 
+   || (cfg_tag.common.led_en       != cfg_node.common.led_en) 
+   || (cfg_tag.common.ble_en       != cfg_node.common.ble_en) 
+   || (cfg_tag.common.uwb_mode     != cfg_node.common.uwb_mode) 
+   || (cfg_tag.common.fw_update_en != cfg_node.common.fw_update_en))
+   {
+      HAL_Print("low_power_en        cfg_tag=%d : cfg_node=%d\n", cfg_tag.low_power_en,     cfg_node.low_power_en); 
+      HAL_Print("meas_mode           cfg_tag=%d : cfg_node=%d\n", cfg_tag.meas_mode,        cfg_node.meas_mode); 
+      HAL_Print("loc_engine_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.loc_engine_en,    cfg_node.loc_engine_en); 
+      HAL_Print("common.led_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.led_en,    cfg_node.common.led_en); 
+      HAL_Print("common.ble_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.ble_en,    cfg_node.common.ble_en); 
+      HAL_Print("common.uwb_mode     cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.uwb_mode,  cfg_node.common.uwb_mode); 
+      HAL_Print("common.fw_update_en cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.fw_update_en, cfg_node.common.fw_update_en);  
+      HAL_Print("\nConfiguration failed.\n\n");
+   }
+   else
+   {
+      HAL_Print("\nConfiguration succeeded.\n\n");
+   }
+   
+   dwm_loc_data_t loc;
+   dwm_pos_t pos;
+   loc.p_pos = &pos;
+   while(1)
+   {      
+      HAL_Print("Wait %d ms...\n", wait_period);
+      HAL_Delay(wait_period);        
+   
+      HAL_Print("dwm_loc_get(&loc):\n");
+      if(dwm_loc_get(&loc) == RV_OK)
+      {
+         HAL_Print("\t[%d,%d,%d,%u]\n", loc.p_pos->x, loc.p_pos->y, loc.p_pos->z,
+               loc.p_pos->qf);
 
-    //---------- Creation des threads
-    pthread_t t_spf;
-    if(pthread_create(&t_spf, NULL, spf_thread, &mission_se) == -1) {
-        perror("pthread_create");
-        return EXIT_FAILURE;
-    }  
-
-    // wait for thread to execute 
-    //void ** returned_path;
-
-    if (pthread_join(t_spf, NULL)) {
-        perror("pthread_join");
-        return EXIT_FAILURE;
-    }
-    // end of thread
-    
-    for(int i=0;i<mission_se.path->size;i++){
-        printf("- %d\n",mission_se.path->dest[i]->id);    
-    }
-    
-    printf("fin du main\n");
-    return EXIT_SUCCESS;
-    return 0;
-}
-
-
-void system_loop(){
-    // Init varibles
-
-    // get mission
-
-    // sensor data initiation
-
-    
+         for (i = 0; i < loc.anchors.dist.cnt; ++i) 
+         {
+            HAL_Print("\t%u)", i);
+            HAL_Print("0x%llx", loc.anchors.dist.addr[i]);
+            if (i < loc.anchors.an_pos.cnt) 
+            {
+               HAL_Print("[%d,%d,%d,%u]", loc.anchors.an_pos.pos[i].x,
+                     loc.anchors.an_pos.pos[i].y,
+                     loc.anchors.an_pos.pos[i].z,
+                     loc.anchors.an_pos.pos[i].qf);
+            }
+            HAL_Print("=%u,%u\n", loc.anchors.dist.dist[i], loc.anchors.dist.qf[i]);
+         }
+      }
+   }
+   
+   return 0;   
 }
