0c76167ca9b105334c5750282d8f9ead9ec53164
Sami_twirtee_poste <sami.ezzerouali@hotmail.com>
2019-04-15 16:07:38 +0200

    Working FLOAT TO HEX

--- bandMaster/main.c
+++ bandMaster/main.c
@@ -12,6 +12,7 @@
 #include "./include/measurement_randomization.h"
 #include "./include/spf_thread.h"
 #include "./include/twirtee.h"
+#include <math.h>
 
 #include <pthread.h>
 #include <stdio.h>
@@ -22,6 +23,7 @@
 
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
+#define _CAN_MSG_LEN 8
 
 char* out_tracking_file = "./output_txt/path.txt";
 char* out_mission_file = "./output_txt/mission.txt";
@@ -39,11 +41,27 @@ typedef struct simu_param(){
     T_odo *odometry;
 } simu_param;
 */
-
-int add_to_can_buffer(char* payload,can_shared* can_buff){
+unsigned int gen_can_message(unsigned int payload,int i){
     //strncpy(temp_data_buffer,&can_buff->data,can_buff->available*sizeof(char)*8);
     //strcat(can_buff->data,payload);
-    return 0;
+    char base[5][13]={"00112233","12345678","ABCDEF","DEADBEEF","99999999"};
+    // payload = base[i];
+    float f_base[]={9.81f,1.6184f,3.14f,2.74f,1.609f};
+    //ftoa(f_base[i],payload,3);
+    //payload = (unsigned char*)&f_base[i];
+    //char id[12]="137#";
+    memcpy(&payload,&f_base[i],4);
+    // strcat(id,payload);
+
+
+    // union {
+    //     float f_data;
+    //     unsigned char c_data[sizeof(float)]
+    // }cast;
+    // cast.f_data=f_base[i];
+    // payload=cast.c_data;
+    
+    return payload;
 }
 
 int main() {
@@ -51,7 +69,7 @@ int main() {
     //###############################################
     //                 output File                  #
     //###############################################
-
+    
     //###############################################
     //                 Create threads               #
     //###############################################
@@ -68,21 +86,42 @@ int main() {
     //###############################################
     // CAN PROTOCOLE READ
 
+    union {
+        float f_data;
+        unsigned int i_data[4];
+    }caster;
     // char* can_name[]={"read_can", "can1"};
     // start_thread(&t_can_read, NULL, read_can, can_name);
     // // wait for thread to execute
     // end_thread(t_can_read, NULL);
     
     can_shared* can_buff = safe_alloc(sizeof(can_shared));
-    
+    can_buff->available=0;
     start_thread(&t_can_write, NULL, write_can, can_buff);
     // wait for thread to execute
+    char k;
+    unsigned int test_payload;
+    int i = 0;
+    caster.f_data=3.14f;
+    memcpy (&test_payload, &caster.f_data, 4);  
+    //test_payload = (unsigned int) caster.f_data;
+    printf("casted float : %08x %f\n",test_payload,caster.f_data);
+    
+
     while(true){
-
-        //char payloads[100][8];
-        scanf("Write can message %s\n",can_buff->data[can_buff->available]);
-        printf("Payload : %s\n",can_buff->data[can_buff->available]);
-
+        pthread_mutex_lock(&can_buff->mutex);
+        test_payload=gen_can_message(test_payload,can_buff->available);
+        //printf("can_gen [%d] : %s\n",can_buff->available,test_payload);
+        //printf("Enter message to send : ");
+        //fgets(can_buff->data[can_buff->available],12,stdin);
+        //printf("\n");
+        memcpy(&test_payload,&can_buff->data[can_buff->available],4);
+        // strncpy(can_buff->data[can_buff->available],test_payload,_CAN_MSG_LEN*sizeof(char));
+        
+        printf("Payload : %x\n",can_buff->data[can_buff->available]);
+        can_buff->available++;
+        pthread_mutex_unlock(&can_buff->mutex);
+        my_delay(2);
         // else{
         //     printf("Eror adding payload to buffer, return : %d\n",add_to_can_buffer(payload,can_buff));
         // }
