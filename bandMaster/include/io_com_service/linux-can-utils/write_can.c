809ce980cdd0f09756e49f81b3d88e177c01aa43
Esami <sami.ezzerouali@hotmail.com>
2019-06-14 15:22:13 +0200

    delay canread

--- bandMaster/include/io_com_service/linux-can-utils/write_can.c
+++ bandMaster/include/io_com_service/linux-can-utils/write_can.c
@@ -193,7 +193,7 @@ void* write_can(void* can_buffer)
         }
         can_buff->available=0;
         pthread_mutex_unlock(&can_buff->mutex);
-        delay(1000);
+        delay(1);
         //return 0;
     }
 }
