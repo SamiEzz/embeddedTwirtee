cb7279938100d10922e3b4c03fbaeced38b159bb
Esami <sami.ezzerouali@hotmail.com>
2019-06-24 14:08:13 +0200

    test_can_ reset can signal term+up

--- bandMaster/include/io_com_service/linux-can-utils/read_can.c
+++ bandMaster/include/io_com_service/linux-can-utils/read_can.c
@@ -251,7 +251,7 @@ int read_can(void* _can_shared){
 	FILE *logfile = NULL;
 
 	//signal(SIGTERM, sigterm);
-	signal(SIGHUP, sigterm);
+	//signal(SIGHUP, sigterm);
 	signal(SIGINT, sigterm);
 
 	last_tv.tv_sec  = 0;
