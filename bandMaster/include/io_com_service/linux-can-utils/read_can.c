e77c798988f8a82735be3c0ecee4c4406832cc7b
Esami <sami.ezzerouali@hotmail.com>
2019-06-25 11:28:04 +0200

    test read can range

--- bandMaster/include/io_com_service/linux-can-utils/read_can.c
+++ bandMaster/include/io_com_service/linux-can-utils/read_can.c
@@ -758,7 +758,7 @@ int read_can(void* _can_shared){
 				//sprint_canframe(can_buff->data[can_buff->available],&frame,view);
 				//sprintf(can_buff->data[can_buff->available],"%x%x%x%x\0",frame.data[0],frame.data[1],frame.data[2],frame.data[3]);
 				sprint_canframe(can_buff->data[can_buff->available],&frame,view);
-				for(int i=4;i<13;i++){
+				for(int i=4;i<12;i++){
 					printf("%c",can_buff->data[can_buff->available][i]);
 					can_buff->data[can_buff->available][i-4]=can_buff->data[can_buff->available][i];
 				}
