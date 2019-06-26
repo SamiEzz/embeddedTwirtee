1cf37a9b28653b18ea2d37a5d2cc73ef9076725e
Esami <sami.ezzerouali@hotmail.com>
2019-06-26 15:13:30 +0200

    working almost perfect can

--- bandMaster/include/io_com_service/linux-can-utils/read_can.c
+++ bandMaster/include/io_com_service/linux-can-utils/read_can.c
@@ -770,7 +770,7 @@ int read_can(void* _can_shared){
 					printf("\nID#DATA %x#%s\n" ,can_buff->id[z],can_buff->data[z]);
 				}
 				
-				printf("\n============ xdata/last : %x/%x \t id/last %x/%x",xdata,last_msg , can_buff->id[can_buff->available],last_id);
+				//printf("\n============ xdata/last : %x/%x \t id/last %x/%x",xdata,last_msg , can_buff->id[can_buff->available],last_id);
 				//printf("\n========= available : %d\n",can_buff->available);
 				if((xdata==last_msg) && (can_buff->id[can_buff->available]==last_id)){
 					printf("\nsame can message repeated\n");
