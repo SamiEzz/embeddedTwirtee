af4a0c0d38d74031a58f5186cccec380462333bc
Esami <sami.ezzerouali@hotmail.com>
2019-06-28 17:27:40 +0200

    test_can_write_sof _3

--- bandMaster/include/io_com_service/io_com_service.c
+++ bandMaster/include/io_com_service/io_com_service.c
@@ -27,15 +27,7 @@
 
 
 #define SYSTEM_CMD_STOP 1
-void io_simulation(COM_CONFIG* cfg,int y){
-    io_write(io_vitesse_V,y,cfg);
-    io_write(io_omega_W,y+float2uint32(9.89),cfg);
-    io_write(io_odometrie_left,y+0x1995,cfg);
-    io_write(io_odometrie_right,y+0x0606,cfg);
-//    io_write(0,float2uint32(6.28),cfg);
-    
-    
-}
+
 void io_service_main(){
     pthread_t t_io_read_can;
     pthread_t t_io_write_can;
@@ -82,10 +74,10 @@ void io_service_main(){
             
         }
         for(int y=0;y<200;y++){
-            io_simulation(cfg,y);
-            //pthread_mutex_init(&(pipeline_can.mutex),NULL);
-            //io_can_write_engine(cfg,&pipeline_can);
-           
+            io_write(io_vitesse_V,y,cfg);
+            io_write(io_omega_W,y,cfg);
+            io_write(io_odometrie_left,y+0x1995,cfg);
+            io_write(io_odometrie_right,y+0x0606,cfg);
         }
         can_read_pipeline.available=0;        
     }
